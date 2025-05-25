a622e8eae2ea83e1752652683bb04c8cbc636e66
hungnq131002 <hungnq131002@gmail.com>
2025-05-25 09:09:00 +0700

    feat: add function to connect metamask

--- projects/faucet_final/src/App.js
+++ projects/faucet_final/src/App.js
@@ -1,6 +1,33 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
+  
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  })
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+
+      if (provider) {
+        provider.request({ method: 'eth_requestAccounts' })
+        setWeb3Api({
+          provider,
+          web3: new Web3(provider)
+        });
+      }
+      else {
+        console.error('Please install MetaMask or another Ethereum provider');
+      }
+    }
+    loadProvider();
+  }, []);
+  
   return (
     <div className='faucet-wrapper'>
       <div className='faucet'>
